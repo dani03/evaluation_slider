085e87c6205ec29fb96d3ed3a545df4848f77044
dani03 <lesamessipa@gmail.com>
2020-12-18 16:43:17 +0100

    maj du readme et des commentaires

--- js/app.js
+++ js/app.js
@@ -3,13 +3,13 @@ const arrowLeft = document.body.querySelector('#arrow-left');
 const arrowRight = document.body.querySelector('#arrow-right');
 
 var tableauImage = ["photo1.jpeg", "photo2.jpeg", "photo3.jpeg", "photo4.jpeg"];
+
 // recuperation de l'image du slider 
 const imageInSlide = document.querySelector('.box-slide-image');
-let indexImage = 0;
 //recuperation div indicateur 
 var divIndicators = document.querySelector("#box-slide-blockIndicators");
 
-// ajouter le nombres d'indicateur au nombres d'images
+// ajouter le nombre d'indicateur aux nombres d'images
 for (let i = 0; i < tableauImage.length; i++) {
   divIndicators.innerHTML += `<span id="${tableauImage[i]}" class="box-slide-indicator"> ° </span>`;
 }
@@ -17,6 +17,7 @@ for (let i = 0; i < tableauImage.length; i++) {
 //on affecte les indicateurs aux images 
 divIndicators.addEventListener('click', e => imageInSlide.src = `assets/images/${e.target.id}`)
 
+let indexImage = 0;
 arrowRight.addEventListener('click', (e) => {
   if (indexImage < tableauImage.length - 1) {
     imageInSlide.src = `assets/images/${tableauImage[indexImage]}`;
