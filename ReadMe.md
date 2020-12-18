6a8e47ce47078a86c8c9766a0a4806c57d2f9e11
dani03 <lesamessipa@gmail.com>
2020-12-18 16:27:59 +0100

    ajout de quelques commentaires

--- ReadMe.md
+++ ReadMe.md
@@ -1,4 +1,4 @@
-slider photos 
+# slider photos 
 cloner le projet installer lancer 
 lancer le projet avec le fichier index.html
 le dossier "js" contient le fichier javascript
--- js/app.js
+++ js/app.js
@@ -9,14 +9,14 @@ let indexImage = 0;
 //recuperation div indicateur 
 var divIndicators = document.querySelector("#box-slide-blockIndicators");
 
+// ajouter le nombres d'indicateur au nombres d'images
 for (let i = 0; i < tableauImage.length; i++) {
   divIndicators.innerHTML += `<span id="${tableauImage[i]}" class="box-slide-indicator"> ° </span>`;
 }
-divIndicators.addEventListener('click', e => {
-  console.log(e.target.id);
-  imageInSlide.src = `assets/images/${e.target.id}`;
 
-})
+//on affecte les indicateurs aux images 
+divIndicators.addEventListener('click', e => imageInSlide.src = `assets/images/${e.target.id}`)
+
 arrowRight.addEventListener('click', (e) => {
   if (indexImage < tableauImage.length - 1) {
     imageInSlide.src = `assets/images/${tableauImage[indexImage]}`;
